98e3efb922717152a650e9bb1fc5ea84e2f60056
stijn <s.j.rotman@tilburguniversity.edu>
2025-12-05 16:43:46 +0100

    Change VimTeX settings.

--- init.lua
+++ init.lua
@@ -495,7 +495,8 @@ require('lazy').setup({
     lazy = false, -- we don't want to lazy load VimTeX
     init = function()
       -- VimTeX configuration goes here, e.g.
-      vim.g.vimtex_view_method = 'mupdf'
+      vim.g.vimtex_view_method = 'zathura'
+      vim.g.vimtex_quickfix_open_on_warning = 0
 
       -- Compile with LuaLaTeX
       vim.keymap.set('n', '<leader>lb', function()
